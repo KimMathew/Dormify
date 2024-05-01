d641da5fbb74770995162a4530649bcb3aad2bf7
Adrian <reinieradrianluna@gmail.com>
2024-05-01 15:29:25 +0800

    added admin liability function

--- AdminLiabilities.Designer.cs
+++ AdminLiabilities.Designer.cs
@@ -28,19 +28,204 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label2 = new System.Windows.Forms.Label();
+            this.asignee = new System.Windows.Forms.ComboBox();
+            this.name = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.description = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.price = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dueDate = new System.Windows.Forms.TextBox();
+            this.submit = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.removeLiab = new System.Windows.Forms.TextBox();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label7 = new System.Windows.Forms.Label();
+            this.specificLiab = new System.Windows.Forms.TextBox();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.viewSpecified = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(7, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Assignee";
+            // 
+            // asignee
+            // 
+            this.asignee.FormattingEnabled = true;
+            this.asignee.Items.AddRange(new object[] {
+            "general",
+            "adrian",
+            "kim",
+            "gian"});
+            this.asignee.Location = new System.Drawing.Point(114, 43);
+            this.asignee.Name = "asignee";
+            this.asignee.Size = new System.Drawing.Size(176, 21);
+            this.asignee.TabIndex = 20;
+            // 
+            // name
+            // 
+            this.name.Location = new System.Drawing.Point(114, 70);
+            this.name.Multiline = true;
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(176, 24);
+            this.name.TabIndex = 21;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(7, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 13);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "Liability Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(7, 100);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 13);
+            this.label4.TabIndex = 23;
+            this.label4.Text = "Liability Description";
+            // 
+            // description
+            // 
+            this.description.Location = new System.Drawing.Point(114, 100);
+            this.description.Multiline = true;
+            this.description.Name = "description";
+            this.description.Size = new System.Drawing.Size(176, 24);
+            this.description.TabIndex = 24;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Poppins Medium", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(96)))), ((int)(((byte)(197)))));
-            this.label1.Location = new System.Drawing.Point(251, 214);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(7, 128);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(230, 48);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Liabilities Here";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 25;
+            this.label1.Text = "Liability Price";
+            // 
+            // price
+            // 
+            this.price.Location = new System.Drawing.Point(114, 130);
+            this.price.Multiline = true;
+            this.price.Name = "price";
+            this.price.Size = new System.Drawing.Size(176, 21);
+            this.price.TabIndex = 26;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(7, 153);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 13);
+            this.label5.TabIndex = 27;
+            this.label5.Text = "Due date";
+            // 
+            // dueDate
+            // 
+            this.dueDate.Location = new System.Drawing.Point(114, 157);
+            this.dueDate.Multiline = true;
+            this.dueDate.Name = "dueDate";
+            this.dueDate.Size = new System.Drawing.Size(176, 21);
+            this.dueDate.TabIndex = 28;
+            // 
+            // submit
+            // 
+            this.submit.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.submit.Location = new System.Drawing.Point(150, 184);
+            this.submit.Name = "submit";
+            this.submit.Size = new System.Drawing.Size(114, 43);
+            this.submit.TabIndex = 29;
+            this.submit.Text = "Submit";
+            this.submit.UseVisualStyleBackColor = true;
+            this.submit.Click += new System.EventHandler(this.submit_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(40, 271);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(84, 13);
+            this.label6.TabIndex = 30;
+            this.label6.Text = "Remove Liability";
+            // 
+            // removeLiab
+            // 
+            this.removeLiab.Location = new System.Drawing.Point(23, 287);
+            this.removeLiab.Name = "removeLiab";
+            this.removeLiab.Size = new System.Drawing.Size(112, 20);
+            this.removeLiab.TabIndex = 31;
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(43, 313);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(81, 24);
+            this.btn2.TabIndex = 32;
+            this.btn2.Text = "remove";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.remove_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(307, 43);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(414, 184);
+            this.dataGridView1.TabIndex = 33;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(473, 243);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(108, 13);
+            this.label7.TabIndex = 34;
+            this.label7.Text = "View Specific Liability";
+            // 
+            // specificLiab
+            // 
+            this.specificLiab.Location = new System.Drawing.Point(383, 259);
+            this.specificLiab.Name = "specificLiab";
+            this.specificLiab.Size = new System.Drawing.Size(290, 20);
+            this.specificLiab.TabIndex = 35;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(362, 313);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(333, 142);
+            this.dataGridView2.TabIndex = 36;
+            // 
+            // viewSpecified
+            // 
+            this.viewSpecified.Location = new System.Drawing.Point(500, 284);
+            this.viewSpecified.Name = "viewSpecified";
+            this.viewSpecified.Size = new System.Drawing.Size(50, 23);
+            this.viewSpecified.TabIndex = 37;
+            this.viewSpecified.Text = "view";
+            this.viewSpecified.UseVisualStyleBackColor = true;
+            this.viewSpecified.Click += new System.EventHandler(this.viewSpecified_Click);
             // 
             // AdminLiabilities
             // 
@@ -48,17 +233,55 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(228)))), ((int)(((byte)(253)))));
             this.ClientSize = new System.Drawing.Size(733, 477);
+            this.Controls.Add(this.viewSpecified);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.specificLiab);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.removeLiab);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.submit);
+            this.Controls.Add(this.dueDate);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.price);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.description);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.name);
+            this.Controls.Add(this.asignee);
+            this.Controls.Add(this.label2);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "AdminLiabilities";
             this.Text = "AdminLiabilities";
+            this.Load += new System.EventHandler(this.AdminLiabilities_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox asignee;
+        private System.Windows.Forms.TextBox name;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox description;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox price;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox dueDate;
+        private System.Windows.Forms.Button submit;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox removeLiab;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox specificLiab;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button viewSpecified;
     }
 }
--- AdminLiabilities.cs
+++ AdminLiabilities.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using CsvHelper;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,5 +18,208 @@ namespace Dormify
         {
             InitializeComponent();
         }
+
+        private void AdminLiabilities_Load(object sender, EventArgs e)
+        {
+            LoadLiabilitiesFromCsv();
+        }
+
+        public class Liability
+        {
+            public string Id { get; set; }
+            public string AssigneeName { get; set; }
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public decimal Price { get; set; }
+            public string DueDate { get; set; }
+        }
+
+        private void LoadLiabilitiesFromCsv()
+        {
+            string csvFilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "liabilities.csv");
+
+            // Check if the file exists
+            if (File.Exists(csvFilePath))
+            {
+                using (var reader = new StreamReader(csvFilePath))
+                using (var csv = new CsvReader(reader, new CsvHelper.Configuration.CsvConfiguration(System.Globalization.CultureInfo.CurrentCulture)))
+                {
+
+                    // Read the records from CSV file
+                    var records = csv.GetRecords<Liability>();
+
+                    // Clear existing rows and columns from DataGridView
+                    dataGridView1.Rows.Clear();
+                    dataGridView1.Columns.Clear();
+
+                    // Define DataGridView columns
+                    dataGridView1.Columns.Add("Id", "UniqueID");
+                    dataGridView1.Columns.Add("AssigneeName", "Assignee Name");
+                    dataGridView1.Columns.Add("Name", "Liability Name");
+                    dataGridView1.Columns.Add("Description", "Description");
+                    dataGridView1.Columns.Add("Price", "Price");
+                    dataGridView1.Columns.Add("DueDate", "Due Date");
+
+                    // Populate DataGridView with liability data
+                    foreach (var liability in records)
+                    {
+                        // Add a new row to DataGridView
+                        dataGridView1.Rows.Add(liability.Id, liability.AssigneeName, liability.Name, liability.Description, liability.Price, liability.DueDate);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("CSV file not found.");
+            }
+        }
+
+        private void LoadLiabilitiesByAssigneeFromCsv(string assigneeName)
+        {
+            string csvFilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "liabilities.csv");
+
+            // Check if the file exists
+            if (File.Exists(csvFilePath))
+            {
+                using (var reader = new StreamReader(csvFilePath))
+                using (var csv = new CsvReader(reader, new CsvHelper.Configuration.CsvConfiguration(System.Globalization.CultureInfo.CurrentCulture)))
+                {
+                    // Read the records from CSV file
+                    var records = csv.GetRecords<Liability>().Where(l => l.AssigneeName == assigneeName).ToList();
+
+                    // Clear existing rows and columns from DataGridView
+                    dataGridView2.Rows.Clear();
+                    dataGridView2.Columns.Clear();
+
+                    // Define DataGridView columns
+                    dataGridView2.Columns.Add("Id", "UniqueID");
+                    dataGridView2.Columns.Add("AssigneeName", "Assignee Name");
+                    dataGridView2.Columns.Add("Name", "Liability Name");
+                    dataGridView2.Columns.Add("Description", "Description");
+                    dataGridView2.Columns.Add("Price", "Price");
+                    dataGridView2.Columns.Add("DueDate", "Due Date");
+
+                    // Check if any records were found for the specified assignee
+                    if (records.Any())
+                    {
+                        // Populate DataGridView with filtered liability data
+                        foreach (var liability in records)
+                        {
+                            // Add a new row to DataGridView
+                            dataGridView2.Rows.Add(liability.Id, liability.AssigneeName, liability.Name, liability.Description, liability.Price, liability.DueDate);
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("User not found.");
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("CSV file not found.");
+            }
+        }
+
+
+
+        private void RemoveLiabilityByAssigneeName(string id)
+        {
+            string csvFilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "liabilities.csv");
+
+            // Check if the file exists
+            if (File.Exists(csvFilePath))
+            {
+                // Read all lines from the CSV file
+                List<string> lines = File.ReadAllLines(csvFilePath).ToList();
+
+                // Remove liabilities with the specified assignee name
+                int removedCount = lines.RemoveAll(line => line.Split(',')[0] == id); // Assuming assigneeName is in the second column (index 1)
+
+                if (removedCount > 0)
+                {
+                    // Rewrite the updated data back to the CSV file
+                    File.WriteAllLines(csvFilePath, lines);
+
+                    MessageBox.Show($"Removed {removedCount} liabilities having the unique id: {id}");
+                }
+                else
+                {
+                    MessageBox.Show($"No liabilities having the unique id: {id}");
+                }
+            }
+            else
+            {
+                MessageBox.Show("CSV file not found.");
+            }
+        }
+
+
+
+
+        private void WriteToCsv(Liability liability)
+        {
+            string csvFilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "liabilities.csv");
+
+            using (StreamWriter writer = new StreamWriter(csvFilePath, true))
+            using (CsvWriter csvWriter = new CsvWriter(writer, new CsvHelper.Configuration.CsvConfiguration(System.Globalization.CultureInfo.CurrentCulture)))
+            {
+                csvWriter.WriteRecord(liability);
+                writer.WriteLine(); // Add newline after writing the record
+            }
+        }
+
+
+        
+
+        private void viewSpecified_Click(object sender, EventArgs e)
+        {
+            string viewSpecific = specificLiab.Text;
+            LoadLiabilitiesByAssigneeFromCsv(viewSpecific);
+        }
+
+        private void remove_Click(object sender, EventArgs e)
+        {
+            string idToRemove = removeLiab.Text;
+            RemoveLiabilityByAssigneeName(idToRemove);
+            LoadLiabilitiesFromCsv();
+        }
+
+        private void submit_Click(object sender, EventArgs e)
+        {
+            // Gather liability data from form controls
+            string id = Guid.NewGuid().ToString();
+            string assigneeName = asignee.Text;
+            string userName = name.Text;
+            string desc = description.Text;
+            string priceText = price.Text;
+            string due = dueDate.Text;
+
+            // Check if the user entered a valid number for the liability price
+            if (!decimal.TryParse(priceText, out decimal liabPrice))
+            {
+                MessageBox.Show("Please enter a valid number for the liability price.");
+                return; // Exit the method if the price is not a valid number
+            }
+
+            // Create a Liability object
+            Liability liability = new Liability
+            {
+                Id = id,
+                AssigneeName = assigneeName,
+                Name = userName,
+                Description = desc,
+                Price = liabPrice,
+                DueDate = due
+            };
+
+            // Write liability to CSV file
+            WriteToCsv(liability);
+
+            MessageBox.Show("Liability submitted successfully.");
+
+            LoadLiabilitiesFromCsv();
+        }
+
     }
 }
