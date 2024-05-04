da1049f990ee7e2d8dfd909fe0f4d276a50127d2
coispers <fmosuela12@gmail.com>
2024-05-04 13:42:59 +0800

    partial changes

--- AdminMessages.Designer.cs
+++ AdminMessages.Designer.cs
@@ -28,21 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.showMessages = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // showMessages
+            // 
+            this.showMessages.AutoSize = true;
+            this.showMessages.Location = new System.Drawing.Point(416, 221);
+            this.showMessages.Name = "showMessages";
+            this.showMessages.Size = new System.Drawing.Size(44, 16);
+            this.showMessages.TabIndex = 0;
+            this.showMessages.Text = "label1";
             // 
             // AdminMessages
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(228)))), ((int)(((byte)(253)))));
-            this.ClientSize = new System.Drawing.Size(765, 477);
+            this.ClientSize = new System.Drawing.Size(1020, 587);
+            this.Controls.Add(this.showMessages);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "AdminMessages";
             this.Text = "AdminMessages";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label showMessages;
     }
 }
--- AdminMessages.cs
+++ AdminMessages.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,47 @@ namespace Dormify
 {
     public partial class AdminMessages : Form
     {
+        private static List<string> messageList = new List<string>();
         public AdminMessages()
         {
             InitializeComponent();
         }
+
+
+        private void LoadMessagesFromFile()
+        {
+            string csvFileName = "message.csv";
+            string csvFilePath = Path.Combine(Directory.GetCurrentDirectory(), csvFileName);
+
+            if (File.Exists(csvFilePath))
+            {
+
+                string[] lines = File.ReadAllLines(csvFilePath);
+                if (lines.Length > 0 && !string.IsNullOrWhiteSpace(lines[0]))
+                {
+                    StringBuilder textFormat = new StringBuilder();
+
+                    for (int i = 0; i < lines.Length; i++)
+                    {
+                        textFormat.AppendFormat("{0}. {1}\n\n", i + 1, lines[i]);
+                        showMessages.Text = textFormat.ToString();
+                    }
+                }
+                else
+                {
+                    showMessages.Text = "No messages at the moment.";
+                }
+            }
+            else
+            {
+                showMessages.Text = "No messages at the moment.";
+            }
+
+            if (File.Exists("message.csv"))
+            {
+                messageList = File.ReadAllLines("message.csv").ToList();
+            }
+        }
+
     }
 }
